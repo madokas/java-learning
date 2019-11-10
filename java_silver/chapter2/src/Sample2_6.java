1b74ec045427c780a9dde5e6eb8efec77c670991
yui-madoka <yui@itpm-gk.com>
2019-11-10 15:23:24 +0900

    Sample2_6完了しました。

---
+++ java_silver/chapter2/src/Sample2_6.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Sample2_6 {
+    public static void main(String[] args){
+
+        /*String[] array;         //配列を使用した場合
+        array = new String[1];
+        array[0] = "田中";
+        String name = array[0];
+        System.out.println(name + " : " + array.length);*/
+
+        ArrayList<String> array;        //ArrayListクラスを使用した場合
+        array = new ArrayList<String>(3);
+        array.add("田中");
+        String name = array.get(0);
+        System.out.println(name + " : " + array.size());
+    }
+}
+
+//        arrayList.add(6);
+//        arrayList.add(8);
+//        arrayList.add(1,7);
+
+        //String listStr = arrayList.toString();
+        //System.out.println(listStr);
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            System.out.println(arrayList.get(i));
+//        }
+
