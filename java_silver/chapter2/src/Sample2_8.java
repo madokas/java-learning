4df6a2b8a2bcf30a3470eb559d0e15a2294dc06d
yui-madoka <yui@itpm-gk.com>
2019-11-13 00:00:44 +0900

    Revert "Sample2_8を再作成いたしました。"
    This reverts commit 4342afc4c3bc5c67bb2c4c56c1408dcd77ef2e20.

--- java_silver/chapter2/src/Sample2_8.java
+++ java_silver/chapter2/src/Sample2_8.java
@@ -6,5 +6,6 @@ public class Sample2_8 {
         ArrayList array  = new ArrayList();
         array.add(10); array.add(20);
         System.out.println(array.get(0) + " " + array.get(1));
+
     }
 }
