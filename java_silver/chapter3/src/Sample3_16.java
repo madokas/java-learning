e67ef113c131a3f8edaf4a9ec41662a7c3d26150
yui-madoka <yui@itpm-gk.com>
2019-12-04 00:01:03 +0900

    Sample3_13～Sample3_18まで完了しました

---
+++ java_silver/chapter3/src/Sample3_16.java
@@ -0,0 +1,14 @@
+public class Sample3_16 {
+    public static void main(String[] args) {
+        char c = 'p';
+
+        if (c == 'a') {
+            System.out.println("cの値はaです。");
+        } else if (c == 'b') {
+            System.out.println("cの値はbです。");
+        } else {
+            System.out.println("cの値はaでもbでもありません。");
+            System.out.println("cの値は" + c + "です。");
+        }
+    }
+}
