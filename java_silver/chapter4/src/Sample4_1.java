b48a79af6e6d920279fc188833716aa6056c6193
yui-madoka <yui@itpm-gk.com>
2019-12-10 22:38:29 +0900

    3章　Sample3_19～3_21、 4章　Sample4_1～4_2 完了しました。

---
+++ java_silver/chapter4/src/Sample4_1.java
@@ -0,0 +1,10 @@
+public class Sample4_1 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num < 5) {   //numの値が 5 未満の間以下を繰り返す
+            System.out.println("numの値は " + num + "です。");
+            num++;          //numの値に 1 加算
+        }
+    }
+}
