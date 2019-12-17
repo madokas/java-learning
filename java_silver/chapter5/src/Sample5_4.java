9ebcbfa680a24cfff35e1cc5fee8850a39b41b31
yui-madoka <yui@itpm-gk.com>
2019-12-17 23:06:34 +0900

    Sample5_1～Sample5_4まで完了

---
+++ java_silver/chapter5/src/Sample5_4.java
@@ -0,0 +1,7 @@
+class Test1 {
+    String myPrint(int a, String b) { return "test";}   //①
+    String myPrint(int a, int b) { return "test"; }     //②
+    String myPrint(String a, int b) { return  "test";}  //③
+    //void myPrint(int a, String b) { }                 //④
+    //String myPrint(int x String y) { return "test;]   //⑤
+}
