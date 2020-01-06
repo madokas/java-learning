3d55230a92f62828c544e36577cae196889404c6
yui-madoka <yui@itpm-gk.com>
2020-01-06 21:00:17 +0900

    Sample5_4, Sample5_5 学習完了しました。

--- java_silver/chapter5/src/Sample5_4.java
+++ java_silver/chapter5/src/Sample5_4.java
@@ -1,7 +1,13 @@
-class Test1 {
-    String myPrint(int a, String b) { return "test";}   //①
-    String myPrint(int a, int b) { return "test"; }     //②
-    String myPrint(String a, int b) { return  "test";}  //③
-    //void myPrint(int a, String b) { }                 //④
-    //String myPrint(int x String y) { return "test;]   //⑤
+public class Sample5_4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Test2 {
+    String myPrint(int a, String b) { return "test"; }
+    String myPrint(int a, int b) { return "test"; }
+    String myPrint(String a, int b) { return "test"; }
+    //void myPrint(int a, String b) { }
+    //String myPrint(int x, String y) { return "test; }
 }
